a69c4147a0e63cd0b935b95a78596fc75acba43d
Olga Kiseleva <kiseleva-1996@yandex.ru>
2017-04-17 21:18:21 +0300

    added interface for Database

--- src/main/java/DatabasePrimitive/DatabaseWork.java
+++ src/main/java/DatabasePrimitive/DatabaseWork.java
@@ -1,13 +1,13 @@
 package DatabasePrimitive;
 
-import java.sql.Connection;
-
 public interface DatabaseWork {
     String searchEmployee(int id);
+
     String searchEmployeeJob(String job);
+
     String searchEmployeeDept(String dept);
+
     void AddEmployee(Integer empno, String ename, String job, Integer mgr, String hiredate, Integer salary, String comm, Integer deptno);
+
     void DeleteEmployee(int id);
-
-
 }
